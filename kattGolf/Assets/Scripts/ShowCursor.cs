f5236aeab35f72aaa269ef3df19a120e398714b2
Borgbastaren <39489088+Borgbastaren@users.noreply.github.com>
2018-05-21 22:21:38 +0200

    butik
    Butiken kan göra mer saker

---
+++ kattGolf/Assets/Scripts/ShowCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCursor : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.K))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
