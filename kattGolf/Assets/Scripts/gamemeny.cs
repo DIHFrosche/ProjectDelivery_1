6b32baa4ee4d73e76f126c268f73015643fb7ee5
Borgbastaren <39489088+Borgbastaren@users.noreply.github.com>
2018-05-21 18:07:45 +0200

    Main Menu & Game Menu
    Main Menu fungerar

---
+++ kattGolf/Assets/Scripts/gamemeny.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamemeny : MonoBehaviour {
+
+
+    public bool Gameispaused = false;
+    public GameObject pausemenuUI;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Resume();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Gameispaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Gameispaused = false;   
+    }
+    public void Pause()
+    {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Gameispaused = true;
+    }
+    public void Continue()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void mainmeny()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
