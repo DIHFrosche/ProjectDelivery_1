adeba0fbd744fb00be3db1457ddf880d6768d727
LordHaug3n <ztramzapto@live.com>
2018-05-22 12:34:23 +0200

    level 4  cars

--- kattGolf/Assets/viktor/FlipCar.cs
+++ kattGolf/Assets/viktor/FlipCar.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCar : MonoBehaviour {
-
+public class FlipCar : MonoBehaviour
+{
     float timeRemaining = 1;
     float timeRemainingReset = 0.5f;
 
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
         timeRemaining -= Time.deltaTime; // subtract time each frame
         if (timeRemaining <= 0)
@@ -23,8 +23,9 @@ public class MoveCar : MonoBehaviour {
             timeRemaining = timeRemainingReset;
             // perform your actions (load next scene, move a gameobject, perform garbage clean up etc.)
             //transform.Rotate(Vector3.forward, 90);
-            transform.Rotate(90,0,0);
+            transform.Rotate(90, 0, 0);
             print("iwork");
         }
     }
+    
 }
