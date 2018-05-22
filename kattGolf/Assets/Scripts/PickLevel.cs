51f088ab8cbeacf96289a7eadc39465d55f33f69
Borgbastaren <39489088+Borgbastaren@users.noreply.github.com>
2018-05-22 12:58:52 +0200

    FÄRDIG

--- kattGolf/Assets/Scripts/PickLevel.cs
+++ kattGolf/Assets/Scripts/PickLevel.cs
@@ -11,22 +11,11 @@ public class PickLevel : MonoBehaviour
     }
     public void Level1()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(4);
     }
     public void Level2()
     {
-
+        SceneManager.LoadScene(5);
     }
-    public void Level3()
-    {
 
-    }
-    public void Level4()
-    {
-
-    }
-    public void Level5()
-    {
-
-    }
 }
