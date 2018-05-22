5c7ce21007d4bdf2cb98fd130a7db4b92ddd5382
DIHFrosche <35454882+DIHFrosche@users.noreply.github.com>
2018-05-22 13:23:57 +0200

    done

--- kattGolf/Assets/Scripts/PickLevel.cs
+++ kattGolf/Assets/Scripts/PickLevel.cs
@@ -11,11 +11,15 @@ public class PickLevel : MonoBehaviour
     }
     public void Level1()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(2);
     }
     public void Level2()
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(3);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(4);
     }
 
 }
